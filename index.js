90211bc8b205ab4277168f1c301d8bf759e1fd46
bhumitjaviya54 <93394436+bhumitjaviya54@users.noreply.github.com>
2023-12-28 13:08:44 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// null Data Type
+
+// In the JS, null is a special value that represents empty or unknown value.
+
+let number = null;
+
+console.log(number);
+console.log(typeof number);
+
+// the type should be "null" but it is "object" because null is an object.
+
+// It is a known bug in JS.
+
+// JS defines that null is equavalent to undefined.
+
+console.log(null == undefined); // (==) it is a comparision operator.
